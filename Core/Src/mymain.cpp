8923cc59070f7edd3ff84ad4a8aa61fa48b08227
armaanisfab <armaanisfab@gmail.com>
2024-10-28 16:02:44 +0800

    moved interrupt to myinter.cpp

--- Core/Src/mymain.cpp
+++ Core/Src/mymain.cpp
@@ -14,15 +14,15 @@ int mymain(void)
 {
   LCD_INIT();
 
-  char name[] = "DAS, Ujaan";
+  char name[] = "TEST";
   LCD_DrawString(0, 0, name);
 
   StepperMotor stepper(GPIOA, STP_1_Pin, STP_2_Pin, STP_3_Pin, STP_4_Pin, &htim1);
 
   while (1)
   {
-	  stepper.makeSteps(256, 300, true);
-	  HAL_Delay(100);
+//	  stepper.makeSteps(256, 300, true);
+//	  HAL_Delay(100);
 	  stepper.makeSteps(128, 300, false);
   }
 
