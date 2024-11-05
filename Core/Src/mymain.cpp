ddce8c9d53e3533e78334554bbd3e15bb1a81229
Ujaan Das <ujaandas03@gmail.com>
2024-11-05 09:04:52 +0800

    ldr threshold=move servo

--- Core/Src/mymain.cpp
+++ Core/Src/mymain.cpp
@@ -10,6 +10,7 @@ extern "C" int mymain(void);
 #include <LDR/LDR.h>
 
 extern TIM_HandleTypeDef htim1;
+extern TIM_HandleTypeDef htim3;
 extern ADC_HandleTypeDef hadc1;
 
 void ConvertToDecimalString(uint32_t adcVal, char *buffer) {
@@ -24,6 +25,7 @@ int mymain(void)
   LCD_DrawString(0, 0, name);
 
   StepperMotor stepper(GPIOA, STP_1_Pin, STP_2_Pin, STP_3_Pin, STP_4_Pin, &htim1);
+  CupServo servo(3, &htim3, TIM_CHANNEL_1);
   LDR ldr(&hadc1);
   char buf[12];
 
@@ -38,11 +40,12 @@ int mymain(void)
 
 	  if (ldr.somethingPassed(10)) {
 		  LCD_DrawString(50, 50, "Something detected");
+		  servo.selectCup(0);
 	  } else {
-		  LCD_DrawString(50, 50, "Hmm...");
+		  LCD_DrawString(50, 50, "                    ");
+		  servo.selectCup(1);
 	  }
-
-	  HAL_Delay(100);
+	  HAL_Delay(1000);
   }
 
   return 0;
